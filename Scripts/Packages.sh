960b11e1aab388f580231bb74aa844217b79aae6
LianXia233 <49828512+LianXia233@users.noreply.github.com>
2026-08-09 09:03:59 +0800

    fix: add airpi-fancontrol UPDATE_PACKAGE to Packages.sh for AP3000M

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -84,6 +84,7 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 # FAN789 插件及其他专用硬件插件
 UPDATE_PACKAGE "luci-app-h5000m-fancontrol" "FAN789/luci-app-h5000m-fancontrol" "main"
+UPDATE_PACKAGE "luci-app-airpi-fancontrol" "LianXia233/luci-app-airpi3000m-fancontrol" "main" "all"
 UPDATE_PACKAGE "luci-app-mt5700m" "LianXia233/luci-app-mt5700m" "main"
 UPDATE_PACKAGE "luci-app-h5000m-netmode" "LianXia233/luci-app-h5000m-netmode" "main"
 
